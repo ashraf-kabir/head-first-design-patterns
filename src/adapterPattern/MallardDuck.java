42c9f306482c930726f475d5b42d7ff64c47406a
ashraf-kabir <ashrafkabir95@gmail.com>
2026-02-12 01:22:02 +0600

    adapter pattern init

---
+++ src/adapterPattern/MallardDuck.java
@@ -0,0 +1,13 @@
+package adapterPattern;
+
+public class MallardDuck implements Duck {
+  @Override
+  public void quack() {
+    System.out.println("Quack");
+  }
+
+  @Override
+  public void fly() {
+    System.out.println("I'm flyting");
+  }
+}
