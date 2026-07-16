3abb57168fd282c418d887a3f7f21766bb997606
ashraf-kabir <ashrafkabir95@gmail.com>
2026-07-16 16:20:51 +0600

    template method pattern

---
+++ src/templateMethodPattern/Tea.java
@@ -0,0 +1,12 @@
+package templateMethodPattern;
+
+public class Tea extends CaffeineBeverage {
+  public void brew() {
+    System.out.println("Steeping the tea");
+  }
+
+  @Override
+  public void addCondiments() {
+    System.out.println("Adding Lemon");
+  }
+}
